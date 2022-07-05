ecfc0743347d73264f1c6a3f0726b4bbdb9a2c79
Jacob Smith <jacobsmith1344@gmail.com>
2022-07-05 16:44:04 +0100

    Pings backend for data instead of locally stored JSON

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import './Home.css';
-import teamData from "../../resources/top_20_teams.json"
-import playerData from "../../resources/top_20_players.json"
-import leagueData from "../../resources/league.json"
+//import teamData from "../../resources/top_20_teams.json"
+//import playerData from "../../resources/top_20_players.json"
+//import leagueData from "../../resources/league.json"
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -11,55 +14,76 @@ import Slider from "react-slick";
 
 const Home = () => {
 
-    var settings = {
+    let carouselSettings = {
         dots: true,
         infinite: true,
         slidesToShow: 6,
         slidesToScroll: 4,
     };
 
-    const teamList = []
 
+    const [teamData, setTeamData] = useState([]);
+    const [playerData, setPlayerData] = useState([]);
+    const [leagueData, setLeagueData] = useState([]);
+
+    const getTeamData = async () => {
+        const { data } = await axios.get("http://localhost:8080/api/v2/team/top-20");
+        setTeamData(data);
+        console.log(data);
+    };
+
+    const getPlayerData = async () => {
+        const { data } = await axios.get("http://localhost:8080/api/v2/player/top-20");
+        setPlayerData(data);
+        console.log(data);
+    };
+
+    const getLeagueData = async () => {
+        const { data } = await axios.get("http://localhost:8080/api/v2/league/all");
+        setLeagueData(data);
+        console.log(data);
+    };
+
+    useEffect(() => {
+        getTeamData();
+        getPlayerData();
+        getLeagueData();
+    }, []);
+
+    const teamList = []
     teamData.forEach((el)=>{
         let imgURL = el.logo.split('/')
         imgURL = "https://drive.google.com/uc?export=view&id=" + imgURL[5]
-
-        
         teamList.push(
-            <div className="team-item">
+            <div className="team-item" key={el.teamName}>
                 <p>{el.teamName}</p>
-                <img src={imgURL} alt="image" />
+                <img src={imgURL} alt=""/>
                 <p>{el.rating}</p>
             </div>)
     })
 
     const playerList = []
-
     playerData.forEach((el)=>{
         let imgURL = el.image.split('/')
         imgURL = "https://drive.google.com/uc?export=view&id=" + imgURL[5]
-
         playerList.push(
-            <div className="player-item">
+            <div className="player-item" key={el.combinedName + el.teamName}>
                 <p>{el.combinedName}</p>
                 <p>{el.teamName}</p>
-                <img src={imgURL} alt="image" />
+                <img src={imgURL} alt="" />
                 <p>{el.rating}</p>
             </div>)
     })
 
     
     const leagueList = []
-
     leagueData.forEach((el)=>{
         let imgURL = el.image.split('/')
         imgURL = "https://drive.google.com/uc?export=view&id=" + imgURL[5]
-
-        
         leagueList.push(
-            <div>
+            <div key={el.league}>
                 <p>{el.league}</p>
-                <img src={imgURL} alt="image" />
+                <img src={imgURL} alt=""/>
             </div>)
     })
 
@@ -84,7 +108,7 @@ const Home = () => {
             
             <div className="TeamBanner">
                 <p className="TeamText">TOP TEAMS</p>
-                <Slider className="team-carousel" {...settings}>
+                <Slider className="team-carousel" {...carouselSettings}>
                     {teamList}
                 </Slider>
             </div>
@@ -92,7 +116,7 @@ const Home = () => {
             <div className="line"/>
             <div className="PlayerBanner">
                 <p className="PlayerText">TOP PLAYERS</p>
-                <Slider className="carousel" {...settings}>
+                <Slider className="carousel" {...carouselSettings}>
                     {playerList}
                 </Slider>
             </div>
