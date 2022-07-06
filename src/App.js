758e6891c7425cc890d02ca7839a15742055f770
Jacob Smith <jacobsmith1344@gmail.com>
2022-07-07 00:25:25 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Home from './containers/HomePageBuilder.js';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
